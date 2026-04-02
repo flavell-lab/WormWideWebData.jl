a99b15722b7dbeafaa30858030e23c41ffe46267
urlicht <urlicht@users.noreply.github.com>
2026-04-02 11:40:47 +0100

    format

--- src/api/zenodo.jl
+++ src/api/zenodo.jl
@@ -1,7 +1,7 @@
 function get_zenodo_metadata(
-    record_id::Union{AbstractString, Nothing} = nothing;
-    request_url::Union{AbstractString, Nothing} = nothing,
-    fetch_latest::Bool = true
+    record_id::Union{AbstractString,Nothing} = nothing;
+    request_url::Union{AbstractString,Nothing} = nothing,
+    fetch_latest::Bool = true,
 )
     # 1. input argument
     if isnothing(record_id) && isnothing(request_url)
@@ -12,20 +12,25 @@ function get_zenodo_metadata(
     end
 
     # 2. URL
-    api_url = isnothing(record_id) ? request_url : "https://zenodo.org/api/records/$record_id"
+    api_url =
+        isnothing(record_id) ? request_url : "https://zenodo.org/api/records/$record_id"
 
     # 3. fetch
     response = HTTP.get(api_url)
-    data = JSON.parse(String(response.body), dicttype=Dict{String, Any})
-    
+    data = JSON.parse(String(response.body), dicttype = Dict{String,Any})
+
     # 4. Handle recursion for latest version
     if fetch_latest && haskey(data, "links") && haskey(data["links"], "latest")
         latest_url = data["links"]["latest"]
-        
+
         # Check if we are already at the latest to avoid infinite loops
         if latest_url != api_url
             @info "Redirecting to latest version..."
-            return get_zenodo_metadata(nothing; request_url=latest_url, fetch_latest=false)
+            return get_zenodo_metadata(
+                nothing;
+                request_url = latest_url,
+                fetch_latest = false,
+            )
         end
     end
 
